6b48ded36d8f6336ba80c044f4ce172e7b0ce0d1
inesachour <ines.achour@insat.ucar.tn>
2025-03-07 13:17:14 +0100

    add item themeproperties

--- lib/src/theme/item_theme.dart
+++ lib/src/theme/item_theme.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 
 class ItemTheme {
   double? width;
+  double? height;
   Color? backgroundColor;
   TextStyle? selectedTextStyle;
   TextStyle? unselectedTextStyle;
+  Widget? title;
+  EdgeInsets? padding;
+  EdgeInsets? margin;
 
-  ItemTheme(
-      {this.width,
-      this.backgroundColor,
-      this.selectedTextStyle,
-      this.unselectedTextStyle});
+  ItemTheme({
+    this.width,
+    this.height,
+    this.backgroundColor,
+    this.selectedTextStyle,
+    this.unselectedTextStyle,
+    this.title,
+    this.padding,
+    this.margin,
+  });
 }
