241f43a1341de1b0b2877bfeddd1bb0eb5e09aa4
inesachour <ines.achour@insat.ucar.tn>
2025-03-11 10:46:23 +0100

    documentation

--- lib/src/data/format.dart
+++ lib/src/data/format.dart
@@ -1,9 +1,17 @@
 import 'package:awesome_datetime_picker/src/data/picker_type.dart';
 
+/// Enum representing different date formats for the [AwesomeDatePicker] and [AwesomeDateTimePicker] widgets.
 enum AwesomeDateFormat {
+  /// Day, Month (number), Year format (e.g., 12/05/2023).
   dMy([PickerType.day, PickerType.month_number, PickerType.year]),
+
+  /// Day, Month (text), Year format (e.g., 12 May 2023).
   dMMy([PickerType.day, PickerType.month_text, PickerType.year]),
+
+  /// Year, Month (number), Day format (e.g., 2023/05/12).
   yMd([PickerType.year, PickerType.month_number, PickerType.day]),
+
+  /// Year, Month (text), Day format (e.g., 2023 May 12).
   yMMd([PickerType.year, PickerType.month_text, PickerType.day]);
 
   final List<PickerType> value;
@@ -11,8 +19,11 @@ enum AwesomeDateFormat {
   const AwesomeDateFormat(this.value);
 }
 
+/// Enum representing different time formats for the [AwesomeTimePicker] and [AwesomeDateTimePicker] widgets.
 enum AwesomeTimeFormat {
   //hm([PickerType.hour_12, PickerType.minute]),
+
+  /// 24-hour time format (hour and minute) (e.g., 14:30).
   Hm([PickerType.hour_24, PickerType.minute]);
 
   final List<PickerType> value;
--- lib/src/theme/awesome_time_picker_theme.dart
+++ lib/src/theme/awesome_time_picker_theme.dart
@@ -1,7 +1,13 @@
 import 'package:awesome_datetime_picker/src/theme/item_theme.dart';
 
+/// Theme for the time picker, allowing customization of hour and minute items.
 class AwesomeTimePickerTheme {
+  /// The theme for the hour item in the time picker.
+  /// It customizes the appearance of the hour picker element.
   final ItemTheme? hourTheme;
+
+  /// The theme for the minute item in the time picker.
+  /// It customizes the appearance of the minute picker element.
   final ItemTheme? minuteTheme;
 
   const AwesomeTimePickerTheme({this.hourTheme, this.minuteTheme});
