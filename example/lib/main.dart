cb1e637980fbb4b1d74e1392b56af1d7c8f7ab2a
inesachour <ines.achour@insat.ucar.tn>
2025-03-24 10:33:02 +0100

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,51 +29,37 @@ class _CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text("Awesome DateTime Picker")),
-        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 24),
-        backgroundColor: Colors.blue,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            AwesomeDatePicker(
-              dateFormat: AwesomeDateFormat.dMMy,
-              minDate: AwesomeDate(year: 2025, month: 2, day: 15),
-              maxDate: AwesomeDate(year: 2026, month: 10, day: 10),
-              onChanged: (AwesomeDate date) {
-                print(
-                    "----Date changed : ${date.day}/${date.month}/${date.year}\n");
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            AwesomeTimePicker(
-              timeFormat: AwesomeTimeFormat.hm,
-              //minTime: AwesomeTime(hour: 5, minute: 20),
-              //maxTime: AwesomeTime(hour: 9, minute: 15),
-              onChanged: (AwesomeTime time) {
-                print("----Time changed : ${time.hour}:${time.minute}\n");
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            AwesomeDateTimePicker(
-              dateFormat: AwesomeDateFormat.dMMy,
-              timeFormat: AwesomeTimeFormat.Hm,
-              onChanged: (AwesomeDateTime dateTime) {
-                print(
-                    "----Date time changed : ${dateTime.date.year}/${dateTime.date.month}/${dateTime.date.day} ${dateTime.time.hour}:${dateTime.time.minute}\n");
-              },
-            ),
-          ],
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          AwesomeDatePicker(
+            //dateFormat: AwesomeDateFormat.dMMy,
+            //minDate: AwesomeDate(year: 2025, month: 2, day: 15),
+            //maxDate: AwesomeDate(year: 2026, month: 10, day: 10),
+            onChanged: (AwesomeDate date) {
+              print(
+                  "----Date changed : ${date.day}/${date.month}/${date.year}\n");
+            },
+          ),
+          AwesomeTimePicker(
+            timeFormat: AwesomeTimeFormat.hm,
+            //minTime: AwesomeTime(hour: 5, minute: 20),
+            //maxTime: AwesomeTime(hour: 9, minute: 15),
+            onChanged: (AwesomeTime time) {
+              print("----Time changed : ${time.hour}:${time.minute}\n");
+            },
+          ),
+          AwesomeDateTimePicker(
+            //dateFormat: AwesomeDateFormat.dMMy,
+            //timeFormat: AwesomeTimeFormat.Hm,
+            onChanged: (AwesomeDateTime dateTime) {
+              print(
+                  "----Date time changed : ${dateTime.date.year}/${dateTime.date.month}/${dateTime.date.day} ${dateTime.time.hour}:${dateTime.time.minute}\n");
+            },
+          ),
+        ],
       ),
     );
   }
